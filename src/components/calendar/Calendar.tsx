fdf38f459d03a86d37696b3ccc9bc7eb9ef4abf8
Raman Liashneuski <romanlesh94@gmail.com>
2023-08-16 01:01:05 +0300

    Created Login Form Validation

---
+++ src/components/calendar/Calendar.tsx
@@ -0,0 +1,11 @@
+import {FC} from "react";
+
+const Calendar: FC = () => {
+    return (
+        <div className="calendar">
+
+        </div>
+    );
+}
+
+export default Calendar;
--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,17 +1,52 @@
-import {FC} from "react";
+import React, {ChangeEvent, FC, FormEvent, useState} from "react";
+import axios from "axios";
 
 const Login: FC = () => {
+    const [values, setValues] = useState({
+        login: '',
+        password: '',
+    });
+    const [submitted, setSubmitted] = useState(false);
+
+    const handleLoginInputChange = (event: ChangeEvent<HTMLInputElement>) => {
+        setValues((values) => ({
+            ...values,
+            login: event.target.value,
+        }));
+    };
+
+    const handlePasswordInputChange = (event: ChangeEvent<HTMLInputElement>) => {
+        setValues((values) => ({
+            ...values,
+            password: event.target.value,
+        }));
+    };
+
+    const handleSubmit = (e: FormEvent) => {
+        e.preventDefault();
+        axios.post('https://localhost:5001/log-in', values)
+            .then(response => {
+                console.log("Status: ", response.status);
+                console.log("Data: ", response.data);
+            }).catch(error => {
+                console.error('Something went wrong!', error);
+        });
+        setSubmitted(true);
+    }
+
     return (
         <div className="page">
             <div className="page__container">
-                <form className="page__form">
+                <form className="page__form" onSubmit={handleSubmit}>
                     <h3 className="page__headline">Log in</h3>
                     <label className="page__label">
-                        <p className="page__label-text">Email</p>
+                        <p className="page__label-text">Login</p>
                         <input
                             type="text"
                             className="page__input page__input--email"
-                            placeholder="Enter your email"
+                            placeholder="Enter your login"
+                            value={values.login}
+                            onChange={handleLoginInputChange}
                         />
                     </label>
                     <label className="page__label">
@@ -20,9 +55,11 @@ const Login: FC = () => {
                             type="text"
                             className="page__input page__input--password"
                             placeholder="Enter your password"
+                            value={values.password}
+                            onChange={handlePasswordInputChange}
                         />
                     </label>
-                    <button className="page__button button-cta">Log in</button>
+                    <input type="submit" className="page__button button-cta" value="Log in" />
                 </form>
             </div>
         </div>
